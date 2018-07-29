da29f9266b0fe2b088263774a71ba9c0aadd05e2
RocioMatias05 <rocioa.matias@gmail.com>
2018-07-29 10:56:18 -0500

    Este cambio fue realizado por michel, ya que por problema con git no le permite subir la actualización

--- dashboard/src/data.js
+++ dashboard/src/data.js
@@ -1,20 +1,28 @@
 const init = () => {
   const url = 'https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
   global.getData(url).then((data) => {
-    // console.log(data);
+    // console.log("2.1" + data);
     render(data);
+    // console.log("2.2" + global.getData(url));
+    // console.log("2.3" + url);
   });
-} 
-
-window.global = {
+}; 
+window.global = { 
   // Se genera una función y dentro de ella una constante con la url del JSON para llamarlo con el método fech.
   getData: (url) => {
+    // console.log("2.4" + getData);
     return fetch(url).then(respuesta => respuesta.json());
-  },
+    // console.log("2.4" + respuesta.json());   
+  }, 
   computeStudentsStats: (laboratoria) => {
-    const newArray = [];
+  // console.log("2.5" + laboratoria);
+    // console.log("2.4" + getData);
+    const newArray = [];  
+    // console.log("2.6" + newArray)
     for (let key in laboratoria) {
+      // console.log("2.7" + key)  
       laboratoria[key].generacion;
+      // console.log( laboratoria[key].generacion)
       for (let generaciones in laboratoria[key].generacion) {
         laboratoria[key].generacion[generaciones].estudiantes.forEach(element => {
           const objStudent = {
@@ -23,18 +31,50 @@ window.global = {
             campus: key,
             generation: generaciones,
             stats: {
-
-            }
-
+              
+            }            
+          // console.log("3.0" + (objStudent)) 
           };
+          // console.log( objStudent) 
           newArray.push(objStudent);
         });
       }
-    }    
+    }
+    // console.log( newArray) 
+     
     return newArray;
   },
-  computeGenerationsStats: () => {},
-  sortStudents: () => {},
-  filterStudents: () => {}
+  computeGenerationsStats: (laboratoria) => {
+    const generationsArray = []; 
+    for (let key in laboratoria) {
+      laboratoria[key].generacion;
+      for (let generations in laboratoria[key].generacion) {
+        countGeneration = laboratoria[key].generacion[generations].estudiantes.length ;
+        let generationStudent = laboratoria[key].generacion[generations].estudiantes;
+        let sumPercentage = generationStudent.reduce((prev, current) => {
+          return prev + current.progreso.porcentajeCompletado;
+        }, 0);
+        averageGeneration = Math.round(sumPercentage / countGeneration);
+        const objGenerations = {
+          campus: key,
+          generation: generations, 
+          count: countGeneration,
+          average: averageGeneration
+        };
+        generationsArray.push(objGenerations);
+      
+        console.log(key);
+        console.log(generations);
+        console.log(countGeneration);
+        console.log(sumPercentage); 
+        console.log(averageGeneration); 
+      // console.log(generationsArray); 
+      }
+    }
+    return generationsArray;
+  },
+  sortStudents: (laboratoria) => {},
+
+  filterStudents: (laboratoria) => {},
 };
 window.onload = init;
--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,22 +1,46 @@
 const render = (laboratoria) => {
-  const computerStudents = global.computeStudentsStats(laboratoria);
-  // const generations = global.generations.computeGenerationsStats(laboratoria);
-  let plantillaFinal;
-  computerStudents.forEach(student => {
-    plantillaFinal += structurCard(student.name, student.email, student.campus, student.generation);
-  });
-  document.getElementById('root').innerHTML = plantillaFinal;
-  
-};
+  // console.log("1.0" + laboratoria)
+  // console.log(render)
 
+  const computerStudents = global.computeStudentsStats(laboratoria);
+  // console.log("1.2" + computerStudents);
+  const computerGenerations = global.computeGenerationsStats(laboratoria);
+  // console.log("1.3" + computerGenerations);
+  let plantillaStudent;
+  computerStudents.forEach(student => {
+    // console.log("1.3" +  student );
+    plantillaStudent += structurCard(student.name, student.email, student.campus, student.generation);
+    // console.log("1.4" + student.name  )
+    // console.log("1.5" + student.email  )
+    // console.log("1.6" + student.campus  )
+    // console.log("1.7" + plantillaFinal  )
+  });
+  document.getElementById('root-1').innerHTML = plantillaStudent;
+ 
+  let plantillaGenerations;
+  computerGenerations.forEach(generStud => {
+    plantillaGenerations += structurCard1(generStud.campus, generStud.generation, generStud.average, generStud.count);
+  });
+  document.getElementById('root-2').innerHTML = plantillaGenerations;
+};
 const structurCard = (nombre, email, campus, generacion) => {
   const plantillaCard = `<div>
-                          <h2>${nombre}</h2>
+                          <h4>${nombre}</h4>
                           <p>${email}</p>
                           <p>${campus}</p>
                           <p>${generacion}</p>
                         </div>`;
   return plantillaCard;
 };
+const structurCard1 = (campus, generation, average, count) => {
+  const plantillaCard1 = `<div>
+                            <h2>${campus}</h2>
+                            <p>${generation}</p>
+                            <p>${average}</p>
+                            <p>${count}</p>
+                          </div>`;
+  return plantillaCard1;
+};
+// console.log("1.8" + structurCard )
 // let plantillaGen;
-// generations.forEach(generations => )
+// generations.forEach(generations => ){}}
